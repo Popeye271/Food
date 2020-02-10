ba4b6f1278c37df4283e39dc352c0819bfb847b3
Popeye271 <javierarribasharten@gmail.com>
2020-02-10 14:23:46 -0500

    hola

---
+++ src/srceens/SearchScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Vew, Text, Button, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>
+                This is the Search Screen
+    </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchScreen;
